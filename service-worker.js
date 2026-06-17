dce5349d69e5c16378ea942b1ed825a741a0c8b3
R3WLDCR <285128950+R3WLDCR@users.noreply.github.com>
2026-06-17 21:39:08 +0900

    Make PWA updates load sooner

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "werewolf-gm-v9";
+const CACHE_NAME = "werewolf-gm-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -31,17 +31,14 @@ self.addEventListener("fetch", (event) => {
   if (url.origin !== self.location.origin) return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      const network = fetch(event.request)
-        .then((response) => {
-          if (response.ok) {
-            const clone = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-          }
-          return response;
-        })
-        .catch(() => cached);
-      return cached || network;
-    }),
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
   );
 });
